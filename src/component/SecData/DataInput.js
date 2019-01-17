af00e76f545ab3c255fa3e2d4c78fdc2bc1380f0
jangdn <jangdn@nate.com>
2019-01-17 18:10:22 +0900

    v0.1.1 server 연결 및 text input 전송 완료, percentage bar 완성

--- src/component/SecData/DataInput.js
+++ src/component/SecData/DataInput.js
@@ -1,16 +1,79 @@
 import React, {Component} from 'react'
+import axios from 'axios'
+
 
 const style = {
   DataInput : {
     height: '30%',
+  },
+  rate : {
+    width : '350px',
+    height : '20px',
+    background : 'black'
+
   }
 }
 
+
+
 class DataInput extends Component {
-    render() {
+  state = {
+    title : '',
+    content : '',
+    percentage : 0,
+  }
+
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.title, this.state.content);
+    let data = {}
+    data['title'] =  this.state.title;
+    data['content'] =  this.state.content;
+    console.log(data)
+    
+    axios
+    .post('http://localhost:5000/api/data', data)
+    .then(res => {
+      console.log(res)
+    })
+   
+    let playrate = setInterval(() => { 
+      axios.get('http://localhost:5000/api/datarate')
+      .then(res => {
+        if (res.data >= 100)
+          clearInterval(playrate);
+        console.log(res.data);
+        this.setState({ percentage : res.data });
+      })}, 1000);
+  }  
+  
+  _handleChangeTitle(e) {
+    e.preventDefault();
+    this.setState({
+      title : e.target.value,
+    })
+  }
+
+  _handleChangeContent(e) {
+    e.preventDefault();
+    this.setState({
+      content : e.target.value,
+    })
+  }
+
+  render() {
+      let { percentage } = this.state;
       return (
         <div className="DataInput" style = {style.DataInput}>
-          DataInput
+          <form onSubmit={e => this._handleSubmit(e)} encType="multipart/form-data">
+            <input className="textInput" type="text" onChange={e => this._handleChangeTitle(e)}/>
+            <textarea className="textInput" type="textarea" onChange={e => this._handleChangeContent(e)}/>
+            <button className="submitButton" type="submit" onClick={e => this._handleSubmit(e)}>
+              Upload
+            </button>
+          </form>
+          <div className="rate" style = {{ ...style.rate, width : `${percentage}%`}}></div>
         </div>
       );
     }
